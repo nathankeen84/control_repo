3defe44b03e0172091c632bab606e38710480074
Nathan Keen <nathan.keen@k33n0.com>
2024-01-03 13:14:33 +0000

    c

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,16 @@
 node default {
-#  $default_role = $facts['role']
-#  lookup('role', String[1, default], 'first', $default_role).include
+  $default_role = $facts['role']
+  lookup('role', String[1, default], 'first', $default_role).include
 }
-#node 'puppet' {
-#  include role::master_server
-#}
-#node /^web/ {
-#  include role::app_server
-#}
-#node /^db/ {
-#  include role::db_server
-#}
-#node /dk01.k33n0.com/ {
-#  include role::docker_server
-#}
+node 'puppet' {
+  include role::master_server
+}
+node /^web/ {
+  include role::app_server
+}
+node /^db/ {
+  include role::db_server
+}
+node /dk01.k33n0.com/ {
+  include role::docker_server
+}
