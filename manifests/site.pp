8ff7067fda3e6bf004447e69d4305219cde033a0
Nathan Keen <nathan.keen@k33n0.com>
2024-01-04 13:57:51 +0000

    d

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,15 +5,10 @@ node default {
     owner   => 'root',
   }
 }
-node 'puppet.k33n0.com' {
+node 'puppet.k33n0.com': {
   include role::master_server
 }
-node 'minetest.puppet.vm' {
+node 'minetest.puppet.vm': {
   include role::minecraft_server
 }
-node /^web/ { 
-  include role::app_server
-}
-node /^db/ {
-  include role::db_server
-}
+
